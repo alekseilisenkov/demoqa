c88e91e9e58ac8b5887e4aa1f25da574abed5173
alexeilisenkov <alexlisenkov92@mail.ru>
2022-02-03 21:25:59 +0300

    add build.gradle

---
+++ src/main/java/ru/alexlisenkov/Main.java
@@ -0,0 +1,8 @@
+package ru.alexlisenkov;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("I love you QA GURU");
+    }
+}
---
+++ src/test/java/ru/alexlisenkov/DemoQATest.java
@@ -0,0 +1,12 @@
+package ru.alexlisenkov;
+
+import org.junit.jupiter.api.Test;
+
+public class DemoQATest {
+
+    @Test
+
+    void firstTest() {
+        System.out.println("I love you QA GURU");
+    }
+}
