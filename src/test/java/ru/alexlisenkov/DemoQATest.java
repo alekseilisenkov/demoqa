a9ae96f93c77348283b71edfd3cf4dc63c106152
alexeilisenkov <alexlisenkov92@mail.ru>
2022-02-06 14:59:30 +0300

    My first test

--- src/test/java/ru/alexlisenkov/DemoQATest.java
+++ src/test/java/ru/alexlisenkov/DemoQATest.java
@@ -1,11 +1,23 @@
 package ru.alexlisenkov;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class DemoQATest {
 
-    @Test
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before");
+    }
 
+    @AfterEach
+    void afterEach() {
+        System.out.println("After");
+    }
+
+    @Test
     void firstTest() {
         System.out.println("I love you QA GURU");
     }
---
+++ src/test/java/ru/alexlisenkov/tests/TestBoxHomework.java
@@ -0,0 +1,62 @@
+package ru.alexlisenkov.tests;
+
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class TestBoxHomework {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+
+    @Test
+    void myFirstTest(){
+        open("/automation-practice-form");
+        $("#firstName").setValue("Alexei");
+        $("#lastName").setValue("Lisenkov");
+        $("#userEmail").setValue("alexlis@mail.ru");
+
+        $("#genterWrapper").$(byText("Male")).click(); //наиболее стабильный локатор с выбором родительского элемента
+
+        $("#userNumber").setValue("8999666555");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("February");
+        $(".react-datepicker__year-select").selectOption("2022");
+        $(".react-datepicker__day--006:not(.react-datepicker__day--outside-month)").click();
+
+        $("#subjectsInput").setValue("Physics").pressEnter();
+
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+
+        $("#currentAddress").setValue("Saint-Petersburg");
+
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+
+        $("#submit").click();
+
+        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Alexei Lisenkov"), text("alexlis@mail.ru"), text("Male"), text("8999666555"),
+                text("Saint-Petersburg"), text("NCR Delhi"));
+
+    }
+
+}
