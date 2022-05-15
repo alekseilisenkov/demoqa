4fa78b2be3da14dbfef77ed2c87bfc045b5375a4
alekseilisenkov <alexlisenkov92@mail.ru>
2022-05-15 09:44:05 +0300

    added PageObjects

--- src/test/java/ru/alexlisenkov/pages/components/CalendarComponent.java
+++ src/test/java/ru/alexlisenkov/pages/components/CalendarComponent.java
@@ -9,6 +9,4 @@ public class CalendarComponent {
         $(".react-datepicker__year-select").selectOption(year);
         $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
     }
-
-
 }
