819a95cda289a67b4169d8ad8b61390e93dc8fca
alekseilisenkov <alexlisenkov92@mail.ru>
2022-05-15 15:58:16 +0300

    move variables to another class

--- src/test/java/ru/alexlisenkov/tests/TestData.java
+++ src/test/java/ru/alexlisenkov/tests/TestData.java
@@ -1,6 +1,8 @@
 package ru.alexlisenkov.tests;
 
 public class TestData {
-    public static String firstName = "Alexei",
-                         lastName = "Lisenkov";
+    public static String hobbieOne = "Sports",
+            hobbieTwo = "Music",
+            genderMale = "Male",
+            sincePhysics = "Physics";
 }
