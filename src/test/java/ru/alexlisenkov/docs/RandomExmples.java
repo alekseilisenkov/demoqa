31a30eaf59dffefff8567f87b9e73a97591a6e80
alekseilisenkov <alexlisenkov92@mail.ru>
2022-05-15 12:09:17 +0300

    added RandomUtils & RandomExmples

---
+++ src/test/java/ru/alexlisenkov/docs/RandomExmples.java
@@ -0,0 +1,14 @@
+package ru.alexlisenkov.docs;
+
+import static ru.alexlisenkov.utils.RandomUtils.getRandomMessage;
+import static ru.alexlisenkov.utils.RandomUtils.getRandomString;
+
+public class RandomExmples {
+    public static void main(String[] args) {
+        System.out.println(getRandomString(32));
+
+        String randomMessage = getRandomMessage(32, 42);
+        System.out.println(randomMessage);
+
+    }
+}
